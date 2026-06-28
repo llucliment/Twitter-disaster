3e025757afaa86a86925ea176a1505749553b0c2
Lluc <lclinav@upv.edu.es>
2026-06-28 17:12:21 +0200

    Until adding the head for bertweet

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,10 +4,7 @@ from sklearn.metrics import f1_score
 
 def evaluate(model, loader, criterion, threshold=0.5):
     model.eval()
-    device = torch.device(
-    "cuda" if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available()
-    else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     total_loss = 0
     all_probs = []
     all_labels = []
