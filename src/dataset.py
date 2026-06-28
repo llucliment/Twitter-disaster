1a596b89c4c8cacffac2b35a9222866610ab5e9b
Lluc <lclinav@upv.edu.es>
2026-06-28 15:38:56 +0200

    dataset.py

---
+++ src/dataset.py
@@ -0,0 +1,57 @@
+from torch.utils.data import Dataset
+import torch
+from src.preprocessing import numericalize
+
+PAD_TOKEN = "<PAD>"
+UNK_TOKEN = "<UNK>"
+
+class DisasterTweetsDataset(Dataset):
+    def __init__(self, df, vocab, is_test=False):
+        self.texts = df["text"].tolist()
+        self.vocab = vocab
+        self.is_test = is_test
+
+        if not is_test:
+            self.labels = df["target"].values.astype(np.float32)
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        token_ids = numericalize(self.texts[idx], self.vocab)
+
+        if len(token_ids) == 0:
+            token_ids = [self.vocab[UNK_TOKEN]]
+
+        x = torch.tensor(token_ids, dtype=torch.long)
+
+        if self.is_test:
+            return x
+
+        y = torch.tensor(self.labels[idx], dtype=torch.float32)
+        return x, y
+    
+def collate_batch(batch):
+    texts, labels = zip(*batch)
+
+    lengths = torch.tensor([len(x) for x in texts], dtype=torch.long)
+    padded_texts = pad_sequence(
+        texts,
+        batch_first=True,
+        padding_value=vocab[PAD_TOKEN]
+    )
+
+    labels = torch.stack(labels)
+
+    return padded_texts, lengths, labels
+
+
+def collate_test_batch(batch):
+    lengths = torch.tensor([len(x) for x in batch], dtype=torch.long)
+    padded_texts = pad_sequence(
+        batch,
+        batch_first=True,
+        padding_value=vocab[PAD_TOKEN]
+    )
+
+    return padded_texts, lengths
