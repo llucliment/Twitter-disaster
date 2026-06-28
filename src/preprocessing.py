ffcbcbbcd26ff38e6dfe3d526cfc7bbeaeea15d0
Lluc <lclinav@upv.edu.es>
2026-06-28 15:33:47 +0200

    Initial commit

---
+++ src/preprocessing.py
@@ -0,0 +1,37 @@
+import re
+from collections import Counter
+
+def tokenize(text):
+    def clean_text(text2):
+        text2 = text2.lower()
+        text2 = re.sub(r"https?://\S+|www\.\S+", " url ", text2)
+        text2 = re.sub(r"@\w+", " user ", text2)
+        text2 = re.sub(r"#", "", text2)
+        text2 = re.sub(r"[^a-z0-9\s']", " ", text2)
+        text2 = re.sub(r"\s+", " ", text2).strip()
+        return text2
+
+    return clean_text(text).split()
+
+PAD_TOKEN = "<PAD>"
+UNK_TOKEN = "<UNK>"
+
+def build_vocab(texts, min_freq=2, max_vocab_size=20000):
+    counter = Counter()
+
+    for text in texts:
+        counter.update(tokenize(text))
+
+    vocab = {
+        PAD_TOKEN: 0,
+        UNK_TOKEN: 1
+    }
+
+    for word, freq in counter.most_common(max_vocab_size - 2):
+        if freq >= min_freq:
+            vocab[word] = len(vocab)
+
+    return vocab
+
+def numericalize(text, vocab):
+    return [vocab.get(token, vocab[UNK_TOKEN]) for token in tokenize(text)]
